05e17b4b203f799684afee63f8cebad3785a3287
Khisab Kurniawan <khisabkurniawan@outlook.com>
2025-03-13 12:28:37 +0700

    Init Released

---
+++ src/app/_meta.js
@@ -0,0 +1,18 @@
+export default {
+  index: "Main Page",
+  guides: "Guides",
+  classes: { display: "hidden" },
+  "*": {
+    theme: {
+      footer: false,
+      breadcrumb: false,
+      typesetting: "article",
+      pagination: false,
+    },
+  },
+  contact: {
+    title: "Forum",
+    type: "page",
+    href: "https://arknesia.com",
+  },
+};
---
+++ src/app/layout.jsx
@@ -0,0 +1,40 @@
+import { Footer, Layout, Navbar } from 'nextra-theme-docs'
+import { Head } from 'nextra/components'
+import { getPageMap } from 'nextra/page-map'
+import 'nextra-theme-docs/style.css'
+ 
+export const metadata = {
+  description: 'Official Arknesia Wiki',
+  title: { template: '%s | Official Arknesia Wiki' },
+  metadataBase: new URL('https://arknesia.wiki'),
+}
+
+export default async function RootLayout({ children }) {
+  return (
+    <html lang="en" dir="ltr" suppressHydrationWarning>
+      <Head>
+        {/* Your Additional Tags */}
+      </Head>
+      <body>
+        <Layout
+          navbar={
+            <Navbar
+              logo={<span style={{ fontWeight: 800 }}>Arknesia Wiki</span>}
+              projectLink="https://github.com/khisabkurniawn/arknesia-wiki"
+              chatLink="https://discord.gg/nqbNxtw5QZ"
+            />
+          }
+          pageMap={await getPageMap()}
+          docsRepositoryBase="https://github.com/khisabkurniawn/arknesia-wiki/tree/main"
+          nextThemes={{ disableTransitionOnChange: false, defaultTheme: 'dark' }}
+          navigation={{ prev: false, next: false }}
+          sidebar={{ toggleButton: false }}
+          footer={<Footer />}
+          darkMode={false}
+        >
+          {children}
+        </Layout>
+      </body>
+    </html>
+  )
+}
