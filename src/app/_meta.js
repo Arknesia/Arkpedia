ff71c40ab01c6553b803e327a807403b710a9e42
Khisab Kurniawan <khisabkurniawan@outlook.com>
2025-03-13 17:22:33 +0700

    added a lot of wiki page

--- src/app/_meta.js
+++ src/app/_meta.js
@@ -1,11 +1,23 @@
 export default {
   index: "Main Page",
   Ads: { display: "hidden" },
+  Channels: { display: "hidden" },
   Classes: { display: "hidden" },
+  Commands: { display: "hidden" },
   Copyrights: { display: "hidden" },
+  Currency: { display: "hidden" },
   Guides: "Guides",
+  Infractions: { display: "hidden" },
   ItemTiers: { display: "hidden" },
   Licensing: { display: "hidden" },
+  Parties: { display: "hidden" },
+  Profiles: { display: "hidden" },
+  Ranks: { display: "hidden" },
+  ResourcePacks: { display: "hidden" },
+  SimpleVoiceChat: { display: "hidden" },
+  Voting: { display: "hidden" },
+  World: { display: "hidden" },
+  WorldTime: { display: "hidden" },
   "*": {
     theme: {
       footer: false,
